3ff00879e8266f8ebc4de252f6d75230d272a14d
BrayanRN1114 <0512020022@alumnos.unp.edu.pe>
2024-09-04 00:27:40 -0500

    Previo al desastre

---
+++ src/main/java/com/api/sfacturacion/controllers/InvoicesController.java
@@ -0,0 +1,42 @@
+package com.api.sfacturacion.controllers;
+
+import com.api.sfacturacion.entities.Invoices;
+import com.api.sfacturacion.services.InvoicesService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1")
+public class InvoicesController {
+    private final InvoicesService invoicesService;
+
+    @GetMapping("/invoices")
+    private List<Invoices> findAll() {
+        return invoicesService.findAll();
+    }
+
+    @GetMapping("/invoices/{id}")
+    public Invoices findById(@PathVariable Long id) {
+        return invoicesService.findById(id);
+    }
+
+    @PostMapping("/invoices")
+    public void saveCategories(@RequestBody Invoices invoices) {
+        invoicesService.saveInvoices(invoices);
+    }
+
+    @PutMapping("/invoices")
+    private void editById(@RequestBody Invoices invoices) {
+        invoicesService.saveInvoices(invoices);
+    }
+
+    @DeleteMapping("/invoices/{id}")
+    public void deleteById(@PathVariable Long id) {
+        invoicesService.deleteById(id);
+    }
+
+    
+}
